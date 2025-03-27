dfc477c26cc861414132e35a2bd1b462e5c3bb9a
ssplramlakhan <sspl.ramlakhan2023@gmail.com>
2025-03-27 12:47:03 +0530

    disable slider for 1 item and update the the api for response

--- src/UmbracoProject/App_Code/Controllers/PurchaseFlowController.cs
+++ src/UmbracoProject/App_Code/Controllers/PurchaseFlowController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UmbracoProject.App_Code.Models;
-using UmbracoProject.App_Code.Models.ApiModels;
 using UmbracoProject.App_Code.Services;
 
 namespace UmbracoProject.App_Code.Controllers
@@ -11,7 +10,7 @@ namespace UmbracoProject.App_Code.Controllers
         private readonly IPurchaseFlowService _purchaseFlowService;
         private readonly ILogger<PurchaseFlowController> _logger;
 
-        public PurchaseFlowController(IPurchaseFlowService purchaseFlowService,ILogger<PurchaseFlowController> logger)
+        public PurchaseFlowController(IPurchaseFlowService purchaseFlowService, ILogger<PurchaseFlowController> logger)
         {
             _purchaseFlowService = purchaseFlowService;
             _logger = logger;
@@ -19,7 +18,7 @@ namespace UmbracoProject.App_Code.Controllers
 
         //{domain/purchase-flow/check-address?adresseGUID=""}
         [HttpGet("check-address")]
-        public async Task<ResponseModel> CheckAddress(string adresseGUID)
+        public async Task<IActionResult> CheckAddress(string adresseGUID)
         {
             ResponseModel response = new();
             try
@@ -27,20 +26,21 @@ namespace UmbracoProject.App_Code.Controllers
                 var result = await _purchaseFlowService.CheckAddressAsync(adresseGUID);
                 response.Data = result;
                 response.IsSucceeded = true;
+                return Ok(response);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Error while checking address '{adresseGUID}' controller 'PurchaseFlowController' action 'CheckAddress(string adresseGUID)' Error '{ex.Message}' StackTrace '{ex.StackTrace}' ");
                 response.IsSucceeded = false;
                 response.Error = ex.Message;
-
+                return StatusCode(StatusCodes.Status500InternalServerError, response);
             }
-            return response;
+
         }
 
         //{domain/purchase-flow/get-calendar-dates?orderType=""&adresseguid=""}
         [HttpGet("get-calendar-dates")]
-        public async Task<ResponseModel> GetCalendarDatesAsync(int orderType, string? adresseguid)
+        public async Task<IActionResult> GetCalendarDatesAsync(int orderType, string? adresseguid)
         {
             ResponseModel response = new();
             try
@@ -48,15 +48,16 @@ namespace UmbracoProject.App_Code.Controllers
                 var result = await _purchaseFlowService.GetCalendarDatesAsync(orderType, adresseguid);
                 response.Data = result;
                 response.IsSucceeded = true;
+                return Ok(response);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Error while getting calendar delivary dates for orderType '{orderType}', addressguid '{adresseguid}' controller 'PurchaseFlowController' action 'GetCalendarDatesAsync(int orderType, string? adresseguid)' Error '{ex.Message}' StackTrace '{ex.StackTrace}' ");
                 response.IsSucceeded = false;
                 response.Error = ex.Message;
+                return StatusCode(StatusCodes.Status500InternalServerError, response);
             }
 
-            return response;
         }
     }
 }
--- src/UmbracoProject/App_Code/Services/IPurchaseFlowService.cs
+++ src/UmbracoProject/App_Code/Services/IPurchaseFlowService.cs
@@ -34,16 +34,18 @@ namespace UmbracoProject.App_Code.Services
 
         /// <summary>
         /// Get all possible dates for order your fiber/tv products
+        /// </summary>
+        /// <param name="orderType">    
         /// 1 = (Electricity) Supplier Change (In Figma: "Jeg ønsker at skifte til nef Strøm på min nuværende adresse")
         /// 2 = (Electricity) Move in (In Figma "Jeg flytter ind på leveringsadressen)"
         ///(Be aware with this type of order, the customer can also select dates before today)
         /// 3 = (Fiber/TV) - With this ordertype, you also need to provide the unique adressguid. That paremeter is called "adresseguid".
-        /// </summary>
-        /// <param name="orderType">
-
         /// </param>
-        /// <param name="adresseguid"></param>
+        /// <param name="adresseguid">
+        /// this value need for the tv and fiber orders
+        /// </param>
         /// <returns></returns>
         Task<CalendarDatesApiResponseModel> GetCalendarDatesAsync(int orderType,string? adresseguid);
+
     }
 }
